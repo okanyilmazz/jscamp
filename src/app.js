c76a10a162fe392be781b5c0e0af9d336a98ba09
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2021-11-02 00:30:47 +0300

    Merge branch 'master' of https://github.com/engindemirog/javaScriptStarterKit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 console.log("Merhaba Kodlama.io")
 
 //JS type safe değildir
+<<<<<<< HEAD
 
 let dolarBugun = 9.30
 let dolarDun = 9.20
@@ -36,3 +37,38 @@ console.log("</ul>")
 
 
 
+=======
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+dolarDun = "9.20"
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11
+
+console.log(euroDun)
+
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+for(let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+{/* <ul>
+    <li>Konut kredisi</li>
+    <li>Emlak Konut Kredisi</li>
+    <li>Kamu Konut Kredisi</li>
+</ul> */}
+
+console.log(konutKredileri)
+>>>>>>> 913ee0044458ba0401b6785ff179f907b3fb0c1e
--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /* console.log("sepete eklendi!")
 
 console.log("sepete eklendi!")
@@ -47,10 +48,35 @@ SayHello2();
 
 
 function  addToCart2(productName,quantity,unitPrice) {
+=======
+function addToCart(quantity,productName="Elma") {
+    console.log("Sepete eklendi : ürün : " 
+    + productName + " adet : " + quantity)
+}
+
+//addToCart()
+addToCart(10)
+//addToCart(15)
+
+let sayHello = ()=>{
+    console.log("Hello World!")
+}
+
+sayHello()
+
+let sayHello2 = function () {
+    console.log("Hello World 2")
+}
+
+sayHello2();
+
+function addToCart2(productName, quantity, unitPrice) {
+>>>>>>> 913ee0044458ba0401b6785ff179f907b3fb0c1e
     
 }
 
 addToCart2("Elma",5,10)
+<<<<<<< HEAD
 addToCart2("Armut",2,20)
 addToCart2("Limon",3,15)
 
@@ -58,6 +84,11 @@ addToCart2("Limon",3,15)
 
 
 //obje tanımı = {} ' dir
+=======
+addToCart2("Armut", 2, 20)
+addToCart2("Limon",3,15)
+
+>>>>>>> 913ee0044458ba0401b6785ff179f907b3fb0c1e
 let product1 = {productName:"Elma", unitPrice:10, quantity:5}
 
 function addToCart3(product) {
@@ -71,6 +102,7 @@ addToCart3(product1)
 
 let product2 = {productName:"Elma", unitPrice:10, quantity:5}
 let product3 = {productName:"Elma", unitPrice:10, quantity:5}
+<<<<<<< HEAD
 
 product2=product3
 product2.productName="KARPUZ"
@@ -105,6 +137,19 @@ addToCart4(products)
 
 /*
 function addToCart5(products) {
+=======
+product2 = product3
+product2.productName = "KARPUZ"
+console.log(product3.productName)
+
+let sayi1 = 10
+let sayi2 = 20
+sayi1 = sayi2
+sayi2 = 100
+console.log(sayi1)
+
+function addToCart4(products) {
+>>>>>>> 913ee0044458ba0401b6785ff179f907b3fb0c1e
     console.log(products)
 }
 
@@ -112,6 +157,7 @@ let products = [
     {productName:"Elma", unitPrice:10, quantity:5},
     {productName:"Armut", unitPrice:10, quantity:5},
     {productName:"Karpuz", unitPrice:10, quantity:5}
+<<<<<<< HEAD
 
 ]
 addToCart5(products)
@@ -153,11 +199,22 @@ function add4(bisey,...numbers) { // rest budur.
     let total=0;
     for (let i = 0; i < numbers.length; i++) {
         total=total+numbers[i]
+=======
+]
+
+addToCart4(products)
+
+function add(bisey,...numbers) { //rest
+    let total = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        total = total + numbers[i]
+>>>>>>> 913ee0044458ba0401b6785ff179f907b3fb0c1e
     }
     console.log(total)
     console.log(bisey)
 }
 
+<<<<<<< HEAD
 add4(20,30)
 
 console.log(Math.max(30,10,500,600,120))
@@ -196,3 +253,36 @@ let newProductName,newUnitPrice,newQuantity
 console.log(newProductName)
 console.log(newUnitPrice)
 console.log(newQuantity)
+=======
+add(20,30)
+//add(20,30, 40)
+//add(20,30, 40, 50)
+
+let numbers = [30,10,500,600,120]
+//console.log(...numbers)
+console.log(Math.max(...numbers))
+
+let [icAnadolu,marmara,karadeniz,[icAnadoluSehirleri]] = [
+    {name:"İç Anadolu", population:"20M"},
+    {name:"Marmara", population:"30M"},
+    {name:"Karadeniz", population:"10M"},
+    [
+        ["Ankara","Konya"],
+        ["İstanbul","Bursa"],
+        ["Sinop","Trabzon"],
+    ]
+]
+
+// console.log(icAnadolu.name)
+// console.log(icAnadolu.population)
+console.log(icAnadoluSehirleri)
+
+let newProductName, newUnitPrice, newQuantity
+({productName:newProductName, unitPrice:newUnitPrice, quantity:newQuantity } 
+= {productName:"Elma", unitPrice:10, quantity:5})
+
+console.log(newProductName)
+console.log(newUnitPrice)
+console.log(newQuantity)
+
+>>>>>>> 913ee0044458ba0401b6785ff179f907b3fb0c1e
